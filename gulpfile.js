bf5916bb8e34ba15910b00ae1efbff73c111aea3
Trick93 <576905406@qq.com>
2017-06-05 16:27:21 +0800

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,10 @@
+'use strict'
+
+var gulp = require("gulp");
+var connect = require("gulp-connect");
+
+gulp.task('webserver',function(){
+    connect.server();
+});
+
+gulp.task('default',['webserver']);
